813cb0c9b503438d8c91253b7c5f57b4019262a9
PipocaPorto <jpportocampos@gmail.com>
2023-07-06 15:44:17 -0300

    Adicionando validação de data da consulta

--- consulta.js
+++ consulta.js
@@ -1,36 +1,39 @@
+import { DateTime } from "luxon";
+
+function UserException(message) {
+    this.message = message;
+    this.name = "UserException";
+}
+
 export default class Consulta {
     #cpfPaciente;
     #data;
-    #horaInicial;
-    #horaFinal;
 
-    constructor(cpfPaciente, data, horaInicial, horaFinal) {
-        if (this.#validaCpfPaciente(cpfPaciente)) {
-            this.#cpfPaciente = cpfPaciente;
-        }
-        if (this.#validaData(data)) {
-            this.#data = data;
-        }
+    constructor(paciente, data, horaInicial, horaFinal) {
+        this.#cpfPaciente = paciente.cpf;
         if (this.#validaHoraInicial(horaInicial)) {
             this.#horaInicial = horaInicial;
         }
         if (this.#validaHoraFinal(horaFinal)) {
             this.#horaFinal = horaFinal;
         }
+        if (this.#validaData(data)) {
+            this.#data = data;
+        }
     }
 
     get cpfPaciente() {
         return this.#cpfPaciente;
     }
-    
+
     get data() {
         return this.#data;
     }
-    
+
     get horaInicial() {
         return this.#horaInicial;
     }
-    
+
     get horaFinal() {
         return this.#horaFinal;
     }
@@ -38,31 +41,34 @@ export default class Consulta {
     set cpfPacienteConsulta(cpfPaciente) {
         this.#cpfPaciente = cpfPaciente;
     }
-    
+
     set dataConsulta(data) {
         this.#data = data;
     }
-    
+
     set horaInicialConsulta(horaInicial) {
         this.#horaInicial = horaInicial;
     }
-    
+
     set horaFinalConsulta(horaFinal) {
         this.#horaFinal = horaFinal;
     }
 
-    #validaCpfPaciente(cpfPaciente) {
-        return true
-    }
-    
     #validaData(data) {
-        return true
+        let dataConsulta = DateTime.fromFormat(data, "dd/LL/yyyy");
+        let dataAtual = DateTime.now();
+
+        if (dataConsulta > dataAtual) {
+            return true;
+        }
+
+        throw new UserException("A data da consulta é inválida (precisa ser futura em relação a data atual)");
     }
-    
+
     #validaHoraInicial(horaInicial) {
         return true
     }
-    
+
     #validaHoraFinal(horaFinal) {
         return true
     }
