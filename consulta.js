6d6c08ecf551f9061b4824c437398f92279d2759
PipocaPorto <jpportocampos@gmail.com>
2023-07-07 11:13:09 -0300

    Adicionando validação de datas na consulta

--- consulta.js
+++ consulta.js
@@ -7,18 +7,29 @@ function UserException(message) {
 
 export default class Consulta {
     #cpfPaciente;
-    #data;
+    #dataIni;
+    #dataFin;
 
     constructor(paciente, data, horaInicial, horaFinal) {
+        let dataInicial = "";
+        let dataFinal = "";
+        if (horaFinal <= horaInicial) {
+            throw new UserException("Horário inválido (A hora final deve ser maior que a hora inicial)");
+        }
         this.#cpfPaciente = paciente.cpf;
-        if (this.#validaHoraInicial(horaInicial)) {
-            this.#horaInicial = horaInicial;
+        if (this.#validaHorario(horaInicial)) {
+            dataInicial = data + " " + horaInicial;
         }
-        if (this.#validaHoraFinal(horaFinal)) {
-            this.#horaFinal = horaFinal;
+        if (this.#validaHorario(horaFinal)) {
+            dataFinal = data + " " + horaFinal;
         }
-        if (this.#validaData(data)) {
-            this.#data = data;
+        if (this.#validaData(dataInicial)) {
+            let dataIni = DateTime.fromFormat(dataInicial, "dd/LL/yyyy HHmm");
+            this.#dataIni = dataIni;
+        }
+        if (this.#validaData(dataFinal)) {
+            let dataFin = DateTime.fromFormat(dataFinal, "dd/LL/yyyy HHmm");
+            this.#dataFin = dataFin;
         }
     }
 
@@ -26,36 +37,28 @@ export default class Consulta {
         return this.#cpfPaciente;
     }
 
-    get data() {
-        return this.#data;
+    get dataIni() {
+        return this.#dataIni;
     }
 
-    get horaInicial() {
-        return this.#horaInicial;
-    }
-
-    get horaFinal() {
-        return this.#horaFinal;
+    get dataFin() {
+        return this.#dataFin;
     }
 
     set cpfPacienteConsulta(cpfPaciente) {
         this.#cpfPaciente = cpfPaciente;
     }
 
-    set dataConsulta(data) {
-        this.#data = data;
+    set dataIniConsulta(dataIni) {
+        this.#dataIni = dataIni;
     }
-
-    set horaInicialConsulta(horaInicial) {
-        this.#horaInicial = horaInicial;
-    }
-
-    set horaFinalConsulta(horaFinal) {
-        this.#horaFinal = horaFinal;
+    
+    set dataFinConsulta(dataFin) {
+        this.#dataFin = dataFin;
     }
 
     #validaData(data) {
-        let dataConsulta = DateTime.fromFormat(data, "dd/LL/yyyy");
+        let dataConsulta = DateTime.fromFormat(data, "dd/LL/yyyy HHmm");
         let dataAtual = DateTime.now();
 
         if (dataConsulta > dataAtual) {
@@ -65,11 +68,16 @@ export default class Consulta {
         throw new UserException("A data da consulta é inválida (precisa ser futura em relação a data atual)");
     }
 
-    #validaHoraInicial(horaInicial) {
-        return true
-    }
+    #validaHorario(horario){
+        const min = horario.toString().slice(2);
+        const hora = horario.toString().slice(0, 2);
 
-    #validaHoraFinal(horaFinal) {
-        return true
+        if (parseInt(min) === 0 || parseInt(min) === 15 || parseInt(min) === 30) {
+            if (parseInt(hora) > 8 || parseInt(hora) < 19) {
+                return true;
+            }
+        }
+
+        throw new UserException("Horário inválido (Os horários são definidos de 15 em 15 minutos)");
     }
 }
